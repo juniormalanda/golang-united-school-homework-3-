02d0e2574f3b5cd0b5f82bdea6230913c7843170
Zhainar Zhanekenov <go@email.com>
2022-06-15 23:11:32 +0600

    fix sort map function

--- task03-map.go
+++ task03-map.go
@@ -3,10 +3,17 @@ package homework
 import "sort"
 
 func sortMapValues(input map[int]string) (result []string) {
-	for _, k := range input {
-		result = append(result, k)
+	keys := make([]int, 0)
+
+	for i, _ := range input {
+		keys = append(keys, i)
 	}
 
-	sort.Strings(result)
+	sort.Ints(keys)
+
+	for _, k := range keys {
+		result = append(result, input[k])
+	}
+
 	return
 }
